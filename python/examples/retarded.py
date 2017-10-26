58f911ecef7e3a816c569df1a1dec319e0b5366d
Adriel Casellas <adrielcasellas@gmail.com>
2017-10-26 00:17:30 -0600

    THIS ONE

--- python/examples/retarded.py
+++ python/examples/retarded.py
@@ -70,7 +70,7 @@ def rainbowCycle(strip, wait_ms=20, iterations=5):
 def retard(strip, wait_ms=1, iterations=5):
 	"""Draw rainbow that uniformly distributes itself across all pixels."""
 	for j in range(2):
-		for i in range(2):
+		for i in range(256):
 			for x in range(2):
 				strip.setPixelColor(j % (x + 1), wheel((x+i) & 255))
 		strip.show()
@@ -105,6 +105,10 @@ if __name__ == '__main__':
 	print ('Press Ctrl-C to quit.')
 	while True:
 		print ('Color wipe animations.')
+		retard(strip)
+		retard(strip)
+		retard(strip)
+		retard(strip)
 		retard(strip)
 		retard(strip)
 		retard(strip)
