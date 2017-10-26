05ff4bce1a449820098ca91931af5276df9fcb9a
Adriel Casellas <adrielcasellas@gmail.com>
2017-10-26 01:19:15 -0600

    lol

--- python/examples/retarded.py
+++ python/examples/retarded.py
@@ -67,9 +67,9 @@ def rainbowCycle(strip, wait_ms=1, iterations=1):
 		strip.show()
 		time.sleep(wait_ms/100000.0)
 
-def retard(strip, wait_ms=1, iterations=5):
+def retard(strip, wait_ms=1, iterations=2):
 	"""This one is retarded."""
-	for j in range(256):
+	for j in range(256*iterations):
 		for i in range(1):
 			for x in range(strip.numPixels()):
 				strip.setPixelColor(j % (x + 1), wheel((x+i) & 255))
